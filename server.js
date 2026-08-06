fade2e0463e952f9cf40e6b5bdba0ab70f92a0d1
Minhhacmt071200 <hantm0712@gmail.com>
2026-08-06 17:12:04 +0700

    Update server.js

--- server.js
+++ server.js
@@ -12,8 +12,10 @@ const { generateReportFile } = require('./report');
 
 const app = express();
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '..', 'public')));
-
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 const UPLOAD_ROOT = path.join(__dirname, '..', 'data', 'uploads');
 const REPORT_ROOT = path.join(__dirname, '..', 'data', 'reports');
 
